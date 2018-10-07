8ea14f35405169f6f7cbfa5abc6ed062724766c7
charles <barracudaking@gmail.com>
2018-10-07 17:29:02 +1100

    add 3 sets of commits from franciscofranco

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -45,7 +45,6 @@ static bool sched_boost_active;
 
 static struct delayed_work input_boost_rem;
 static u64 last_input_time;
-#define MIN_INPUT_INTERVAL (150 * USEC_PER_MSEC)
 
 static int set_input_boost_freq(const char *buf, const struct kernel_param *kp)
 {
@@ -164,11 +163,15 @@ static void update_policy_online(void)
 	/* Re-evaluate policy to trigger adjust notifier for online CPUs */
 	get_online_cpus();
 	for_each_online_cpu(i) {
-		/*
-		 * Both clusters have synchronous cores, only update
-		 * the frequency for one core in each cluster.
+				/*
+		cpufreq_update_policy(i);
+		 * both clusters have synchronous cpus
+		 * no need to update the policy for each core
+		 * individually, saving at least one [down|up] write
+		 * and a [lock|unlock] irqrestore per pass
 		 */
-		if (i == 0 || i == 4) {
+		if ((i & 1) == 0) {
+			pr_debug("Updating policy for CPU%d\n", i);
 			cpufreq_update_policy(i);
 		}
 	}
@@ -241,7 +244,7 @@ static void cpuboost_input_event(struct input_handle *handle,
 		return;
 
 	now = ktime_to_us(ktime_get());
-	if (now - last_input_time < MIN_INPUT_INTERVAL)
+	if ((now - last_input_time) < (input_boost_ms * USEC_PER_MSEC))
 		return;
 
 	if (work_pending(&input_boost_work))
